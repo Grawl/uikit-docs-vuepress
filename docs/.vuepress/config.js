f34252ef7008422560f45840a93a8a7c47e19781
Даниил Пронин <mail@grawl.ru>
2019-07-31 22:55:32 +1000

    fix installation page

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,11 @@ module.exports = {
 		md.renderer.rules.fence = function (tokens, idx, options, env, slf) {
 			const token = tokens[idx]
 			const lang = token.info
-			const code = token.content
+			let code = token.content
+			if (code.match(/\[uikit-version]/)) {
+				const UIkitPackage = require('uikit/package')
+				code = code.replace(/\[uikit-version]/g, UIkitPackage.version)
+			}
 			if (lang === 'example') {
 				try {
 					return example(code)
@@ -30,132 +34,5 @@ module.exports = {
 			{ text: 'Changelog', link: 'https://getuikit.com/changelog' },
 			{ text: 'Download', link: 'https://getuikit.com/download' },
 		],
-		sidebar: [
-			{
-				title: 'Getting started',
-				collapsable: false,
-				children: [
-					['introduction', 'Introduction'],
-					['installation', 'Installation'],
-					['less', 'Less'],
-					['sass', 'Sass'],
-					['javascript', 'JavaScript'],
-					['webpack', 'Webpack'],
-					['custom-icons', 'Custom icons'],
-					['avoiding-conflicts', 'Avoiding conflicts'],
-					['rtl', 'RTL support'],
-					['migration', 'Migration'],
-				],
-			},
-			{
-				title: 'Layout',
-				collapsable: false,
-				children: [
-					['container', 'Container'],
-					['flex', 'Flex'],
-					['grid', 'Grid'],
-					['section', 'Section'],
-					['tab', 'Tab'],
-				],
-			},
-			{
-				title: 'Navigation',
-				collapsable: false,
-				children: [
-					['breadcrumb', 'Breadcrumb'],
-					['dotnav', 'Dotnav'],
-					['iconnav', 'Iconnav'],
-					['nav', 'Nav'],
-					['navbar', 'Navbar'],
-					['pagination', 'Pagination'],
-					['slidenav', 'Slidenav'],
-					['subnav', 'Subnav'],
-					['thumbnav', 'Thumbnav'],
-					['totop', 'Totop'],
-				],
-			},
-			{
-				title: 'Containers',
-				collapsable: false,
-				children: [
-					['accordion', 'Accordion'],
-					['article', 'Article'],
-					['card', 'Card'],
-					['column', 'Column'],
-					['description-list', 'Description List'],
-					['form', 'Form'],
-					['lightbox', 'Lightbox'],
-					['list', 'List'],
-					['modal', 'Modal'],
-					['offcanvas', 'Off-canvas'],
-					['sortable', 'Sortable'],
-					['switcher', 'Switcher'],
-					['slider', 'Slider'],
-					['slideshow', 'Slideshow'],
-					['table', 'Table'],
-					['tile', 'Tile'],
-				],
-			},
-			{
-				title: 'Elements',
-				collapsable: false,
-				children: [
-					['alert', 'Alert'],
-					['badge', 'Badge'],
-					['button', 'Button'],
-					['close', 'Close'],
-					['comment', 'Comment'],
-					['countdown', 'Countdown'],
-					['divider', 'Divider'],
-					['drop', 'Drop'],
-					['dropdown', 'Dropdown'],
-					['filter', 'Filter'],
-					['heading', 'Heading'],
-					['icon', 'Icon'],
-					['image', 'Image'],
-					['label', 'Label'],
-					['leader', 'Leader'],
-					['link', 'Link'],
-					['marker', 'Marker'],
-					['notification', 'Notification'],
-					['overlay', 'Overlay'],
-					['placeholder', 'Placeholder'],
-					['progress', 'Progress'],
-					['search', 'Search'],
-					['spinner', 'Spinner'],
-					['svg', 'SVG'],
-					['toggle', 'Toggle'],
-					['tooltip', 'Tooltip'],
-					['upload', 'Upload'],
-					['video', 'Video'],
-				],
-			},
-			{
-				title: 'Style',
-				collapsable: false,
-				children: [
-					['align', 'Align'],
-					['animation', 'Animation'],
-					['background', 'Background'],
-					['base', 'Base'],
-					['cover', 'Cover'],
-					['height', 'Height'],
-					['inverse', 'Inverse'],
-					['margin', 'Margin'],
-					['padding', 'Padding'],
-					['parallax', 'Parallax'],
-					['position', 'Position'],
-					['print', 'Print'],
-					['sticky', 'Sticky'],
-					['text', 'Text'],
-					['transition', 'Transition'],
-					['utility', 'Utility'],
-					['visibility', 'Visibility'],
-					['scroll', 'Scroll'],
-					['scrollspy', 'Scrollspy'],
-					['width', 'Width'],
-				],
-			},
-		]
 	},
 }
